eb1d9408f6bfbfe88e9a994f1a111ff368e6b80a
Akiyoshi Sakaguchi <a-sakaguchi@groovenauts.jp>
2015-11-26 17:31:59 +0900

    Add the processing when the environment variable undefined

--- update-site.sh
+++ update-site.sh
@@ -7,31 +7,43 @@ CLONEDIR=/usr/local/src/html
 PROMPT1="Username for 'https://github.com':"
 PROMPT2="Password for 'https://.+@github.com':"
 
+if [ -z ${GITHUB_BRANCH} ]; then
+  GITHUB_BRANCH=master
+fi
+
 if [ ! -e ${CLONEDIR} ]; then
   mkdir -p ${CLONEDIR}
 fi
 
 if [ -e ${CLONEDIR}/${GITHUB_BRANCH} ]; then
   cd ${CLONEDIR}/${GITHUB_BRANCH}
-  expect -c "
-  set timeout -1
-  spawn git pull
-  expect \"${PROMPT1}\"
-  send -- \"${GITHUB_USERNAME}\n\"
-  expect -re \"${PROMPT2}\"
-  send -- \"${GITHUB_ACCESS_TOKEN}\n\"
-  expect { \"done\" { interact } }
-  "
+  if [ -z ${GITHUB_USERNAME} ] || [ -z ${GITHUB_ACCESS_TOKEN} ]; then
+    git pull
+  else
+    expect -c "
+    set timeout -1
+    spawn git pull
+    expect \"${PROMPT1}\"
+    send -- \"${GITHUB_USERNAME}\n\"
+    expect -re \"${PROMPT2}\"
+    send -- \"${GITHUB_ACCESS_TOKEN}\n\"
+    expect { \"done\" { interact } }
+    "
+  fi
 else
-  expect -c "
-  set timeout -1
-  spawn git clone --branch ${GITHUB_BRANCH} ${GITHUB_REPOSITORY} ${CLONEDIR}/${GITHUB_BRANCH}
-  expect \"${PROMPT1}\"
-  send -- \"${GITHUB_USERNAME}\n\"
-  expect -re \"${PROMPT2}\"
-  send -- \"${GITHUB_ACCESS_TOKEN}\n\"
-  expect { \"done\" { interact } }
-  "
+  if [ -z ${GITHUB_USERNAME} ] || [ -z ${GITHUB_ACCESS_TOKEN} ]; then
+    git clone --branch ${GITHUB_BRANCH} ${GITHUB_REPOSITORY} ${CLONEDIR}/${GITHUB_BRANCH}
+  else
+    expect -c "
+    set timeout -1
+    spawn git clone --branch ${GITHUB_BRANCH} ${GITHUB_REPOSITORY} ${CLONEDIR}/${GITHUB_BRANCH}
+    expect \"${PROMPT1}\"
+    send -- \"${GITHUB_USERNAME}\n\"
+    expect -re \"${PROMPT2}\"
+    send -- \"${GITHUB_ACCESS_TOKEN}\n\"
+    expect { \"done\" { interact } }
+    "
+  fi
 fi
 
 if [ -n ${HUGO} ] && [ -x /usr/local/bin/hugo ]; then
